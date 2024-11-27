2b9ac4ecf1211ef51756e8fef9908da95d534b2a
54ntu <chaudharysantaram8@gmail.com>
2024-11-27 13:24:52 +0545

    cors access is updated

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 const { blogrouter } = require("./routes/blog.routes");
 const app = express();
 
-app.use(
-  cors({
-    origin: ["https://blog-mern-frontend-iota.vercel.app/"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["https://blog-mern-frontend-iota.vercel.app/"],
+//   })
+// );
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
